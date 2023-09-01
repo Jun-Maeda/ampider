a34fcff4de75fc14f95c286af019ca4c37825b60
EC2 Default User <ec2-user@ip-172-31-9-145.ap-northeast-1.compute.internal>
2023-09-01 10:15:37 +0000

    Add Chat function

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,10 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "viteproject": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string"
+    }
+  },
   "auth": {
     "viteprojectc3d2abf6": {
       "AppClientID": "string",

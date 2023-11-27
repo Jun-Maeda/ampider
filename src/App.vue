710f537e049493b655ea730e785266f1f552bc40
20060091 <jun126m@prestigein.com>
2023-11-27 12:56:15 +0900

    fix:メニューリスト内でのプルダウンに変更

--- src/App.vue
+++ src/App.vue
@@ -9,37 +9,92 @@
             <v-btn icon="mdi-account-cog-outline" size="x-large" />
           </div>
           <div v-show="login">
-            <!--        <v-btn prepend-icon="mdi-logout" variant="outlined">-->
-            <!--          ログアウト-->
-            <!--        </v-btn>-->
             <v-btn icon="mdi-logout" size="x-large" @click="signOut" />
           </div>
           <div v-show="!login">
-            <!--        <v-btn prepend-icon="mdi-login" variant="outlined">-->
-            <!--          ログイン-->
-            <!--        </v-btn>-->
             <v-btn icon="mdi-login" size="x-large" />
           </div>
         </v-app-bar>
 
-        <v-navigation-drawer v-model="drawer" absolute temporary>
-          <v-list>
-            <v-list-item-group>
-              <v-list-item
-                v-for="(item, i) in items"
-                :key="i"
-                :value="item"
-                color="primary"
-                rounded="xl"
-                :to="item.to"
-                :prepend-icon="item.icon"
-                :title="item.title"
-              >
-              </v-list-item>
-            </v-list-item-group>
-          </v-list>
-        </v-navigation-drawer>
+        <v-navigation-drawer
+        v-model="drawer"
+        absolute
+        temporary
+    >
+      <v-list>
+        <!-- ホーム -->
+        <v-list-item
+              value="Home"
+              color="primary"
+              rounded="xl"
+              to="/"
+              prepend-icon="mdi-home"
+              title="Home"
+        ></v-list-item>
 
+        <!-- ユーザー設定 -->
+        <v-list-group value="user_settings">
+          <template v-slot:activator="{ props }">
+            <v-list-item
+                v-bind="props"
+                title="ユーザー設定"
+                prepend-icon="mdi-phone-outgoing"
+            >
+            </v-list-item>
+          </template>
+          <v-list-item
+              v-for="(user_setting, i) in user_settings"
+              v-bind="props"
+              :key="i"
+              :value="user_setting"
+              color="primary"
+              rounded="xl"
+              :to="user_setting.to"
+              :prepend-icon="user_setting.icon"
+              :title="user_setting.title"
+          >
+          </v-list-item>
+        </v-list-group>
+
+        <!-- 管理 -->
+        <v-list-group value="admin_menus">
+          <template v-slot:activator="{ props }">
+            <v-list-item
+                v-bind="props"
+                title="管理"
+                prepend-icon="mdi-image-filter-hdr"
+            >
+            </v-list-item>
+          </template>
+          <v-list-item
+              v-for="(admin_menu, i) in admin_menus"
+              v-bind="props"
+              :key="i"
+              :value="admin_menu"
+              color="primary"
+              rounded="xl"
+              :to="admin_menu.to"
+              :prepend-icon="admin_menu.icon"
+              :title="admin_menu.title"
+          >
+          </v-list-item>
+        </v-list-group>
+
+        <v-list-item-group>
+          <v-list-item
+              v-for="(item, i) in items"
+              :key="i"
+              :value="item"
+              color="primary"
+              rounded="xl"
+              :to="item.to"
+              :prepend-icon="item.icon"
+              :title="item.title"
+          >
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
         <v-main>
           <router-view :username="user.username" />
         </v-main>
@@ -60,12 +115,15 @@
 export default {
   data: () => ({
     login: true,
-    items: [
-      { title: 'Home', to: '/', icon: 'mdi-home' },
-      { title: 'chat', to: '/chat', icon: 'mdi-clock' },
-      { title: 'admin', to: '/admin', icon: 'mdi-map-marker' },
-      { title: 'test', to: '/test', icon: 'mdi-map-marker' },
+    user_settings: [
+      { title: '連絡先情報', to: '', icon: 'mdi-clock' },
+      { title: 'テスト', to: '/test', icon: 'mdi-map-marker' },
     ],
+    admin_menus: [
+      { title: '安否確認結果', to: '/admin', icon: 'mdi-clock' },
+      { title: '災害新規作成', to: '', icon: 'mdi-map-marker' },
+    ],
+    items: [{ title: 'chat', to: '/chat', icon: 'mdi-clock' }],
     drawer: false,
     links: ['Home', 'About', 'Contact'],
   }),

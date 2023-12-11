a9f40c07c6960c96fc628f810275d200f1c48a43
20060091 <jun126m@prestigein.com>
2023-12-11 14:45:53 +0900

    add:会社名、拠点名、組織名のセレクトボックス追加

--- src/views/InfoCreate.vue
+++ src/views/InfoCreate.vue
@@ -10,7 +10,17 @@
         </v-row>
         <v-row>
           <v-col cols="12" lg="6">
-            <v-select label="拠点" :items="areas" :rules="rules" v-model="area" />
+            <v-select label="会社" :items="companies" :rules="rules" v-model="company" @click="choiceCompany" />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12" lg="6">
+            <v-select label="拠点" :items="areas" v-model="area" @click="choiceArea" />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12" lg="6">
+            <v-select label="組織" :items="organizations" v-model="organization" @click="choiceOrganization" />
           </v-col>
         </v-row>
         <v-row>
@@ -20,7 +30,10 @@
         </v-row>
         <v-row>
           <v-col cols="12">
-            <v-checkbox v-model="notice" label="通知" color="info" value="info" hide-details></v-checkbox>
+            <v-row>
+              <v-checkbox v-model="notice_flg" label="通知" color="info" hide-details></v-checkbox>
+              <v-checkbox v-model="training_flg" label="避難訓練" color="info" hide-details></v-checkbox>
+            </v-row>
           </v-col>
         </v-row>
         <v-row justify="end">
@@ -36,20 +49,48 @@ export default {
   data: () => ({
     rules: [(v) => !!v || 'この項目は必須です'],
     title: '',
-    areas: ['秋田', '山形', '富山', '東京'],
+    companies: ['全社', 'PI', 'PCS', 'PAD'],
+    company: '',
+    areas: ['test'],
     area: '',
+    organizations: ['test2'],
+    organization: '',
     body_text: '',
-    notice: false,
+    notice_flg: false,
+    training_flg: false,
   }),
   methods: {
     async validate() {
       const { valid } = await this.$refs.form.validate()
       if (valid) {
         let success =
-          '以下の内容で作成しました。\nタイトル:' + this.title + '\n拠点:' + this.area + '\n内容:' + this.body_text + '\n共有:' + this.notice
+          '以下の内容で作成しました。\nタイトル:' +
+          this.title +
+          '\n拠点:' +
+          this.area +
+          '\n内容:' +
+          this.body_text +
+          '\n共有:' +
+          this.notice_flg +
+          '\n訓練:' +
+          this.training_flg
         alert(success)
         this.$refs.form.reset()
       }
+    },
+    choiceCompany(){
+      this.organization = ""
+      this.area = ""
+      this.organizations = []
+      this.areas = []
+    }
+    ,
+    choiceArea() {
+      this.organizations = []
+      this.areas = ['秋田', '山形', '富山', '東京', this.company]
+    },
+    choiceOrganization() {
+      this.organizations = ['PI', 'PCS', 'PAD', this.area]
     },
   },
 }

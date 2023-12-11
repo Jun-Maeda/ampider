c41c244d63d6a80a1b9ed6a06852dd124e5a885d
20060091 <jun126m@prestigein.com>
2023-12-11 15:29:24 +0900

    add:モーダルでの確認画面追加

--- src/views/InfoCreate.vue
+++ src/views/InfoCreate.vue
@@ -32,12 +32,52 @@
           <v-col cols="12">
             <v-row>
               <v-checkbox v-model="notice_flg" label="通知" color="info" hide-details></v-checkbox>
-              <v-checkbox v-model="training_flg" label="避難訓練" color="info" hide-details></v-checkbox>
+              <v-checkbox v-model="training_flg" label="訓練" color="info" hide-details></v-checkbox>
             </v-row>
           </v-col>
         </v-row>
         <v-row justify="end">
+          <v-btn class="mr-3" variant="flat" color="primary" @click="validate">下書き</v-btn>
           <v-btn variant="flat" color="primary" @click="validate">作成</v-btn>
+
+          <v-dialog v-model="dialog" persistent width="auto">
+            <!-- <template v-slot:activator="{ props }">
+              <v-btn @click="validate" color="primary" v-bind="props" >作成</v-btn>
+            </template> -->
+            <v-card>
+              <v-card-title class="text-h5">以下の内容で作成してよろしいですか？</v-card-title>
+              <v-card-text>
+                <v-row>
+                  <v-col class="mr-auto" cols="12" lg="6"> タイトル：{{ title }} </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="12" lg="6"> 会社：{{ company }} </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="12" lg="6"> 拠点：{{ area }} </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="12" lg="6"> 組織：{{ organization }} </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="12"> 本文：{{ body_text }} </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="12">
+                    <v-row>
+                      <v-col cols="12"> 通知：{{ notice_flg }} </v-col>
+                      <v-col cols="12"> 訓練：{{ training_flg }} </v-col>
+                    </v-row>
+                  </v-col>
+                </v-row>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="green-darken-1" variant="text" @click="dialog = false">キャンセル</v-btn>
+                <v-btn color="green-darken-1" variant="text" @click="createForm">作成</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
         </v-row>
       </v-form>
     </div>
@@ -58,16 +98,27 @@ export default {
     body_text: '',
     notice_flg: false,
     training_flg: false,
+    dialog: false,
   }),
   methods: {
     async validate() {
       const { valid } = await this.$refs.form.validate()
+
       if (valid) {
-        let success =
+        this.dialog = true
+        
+      }
+    },
+    createForm(){
+      let success =
           '以下の内容で作成しました。\nタイトル:' +
           this.title +
+          '\n会社:' +
+          this.company +
           '\n拠点:' +
-          this.area +
+          this.area
+          '\n組織:' +
+          this.organization +
           '\n内容:' +
           this.body_text +
           '\n共有:' +
@@ -76,15 +127,14 @@ export default {
           this.training_flg
         alert(success)
         this.$refs.form.reset()
-      }
+        this.dialog = false
     },
-    choiceCompany(){
-      this.organization = ""
-      this.area = ""
+    choiceCompany() {
+      this.organization = ''
+      this.area = ''
       this.organizations = []
       this.areas = []
-    }
-    ,
+    },
     choiceArea() {
       this.organizations = []
       this.areas = ['秋田', '山形', '富山', '東京', this.company]
